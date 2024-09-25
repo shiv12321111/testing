4b3da50c2251555eee4edce1a01fdcfa80150741
shiv12321111 <shivanokalka@gmail.com>
2024-09-25 15:00:26 +0200

    testing.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,76 @@ import streamlit as st
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+	"latitude": [52.25, 50.75, 51.5],
+	"longitude": [5.75, 4.5, 10.5],
+	"start_date": "2000-09-19",
+	"end_date": "2024-09-19",
+	"hourly": ["precipitation", "rain"],
+	"daily": ["temperature_2m_mean", "precipitation_sum", "rain_sum", "snowfall_sum", "precipitation_hours"],
+	"timezone": "auto"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_precipitation = hourly.Variables(0).ValuesAsNumpy()
+hourly_rain = hourly.Variables(1).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["rain"] = hourly_rain
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
+daily_precipitation_sum = daily.Variables(1).ValuesAsNumpy()
+daily_rain_sum = daily.Variables(2).ValuesAsNumpy()
+daily_snowfall_sum = daily.Variables(3).ValuesAsNumpy()
+daily_precipitation_hours = daily.Variables(4).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
+daily_data["precipitation_sum"] = daily_precipitation_sum
+daily_data["rain_sum"] = daily_rain_sum
+daily_data["snowfall_sum"] = daily_snowfall_sum
+daily_data["precipitation_hours"] = daily_precipitation_hours
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+st.line_chart(data=daily_dataframe, x = 'date', y = 'rain_sum')
